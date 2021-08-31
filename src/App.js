d762fba33f8f373443f2e9b9d0f84ea3daf85eba
Gino2B <gbaybay30@gmail.com>
2021-08-30 20:53:50 -0400

    Added pictures for skills and the thumbnail for the projects

--- src/App.js
+++ src/App.js
@@ -3,7 +3,80 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header>
+        <div>Gene Baybay</div>
+        <div>Software Engineer || Junior Developer</div>
+      </header>
+      <div className="about">
+        <div>About Me</div>
+        <div>
+          Based in New York, Passionate and hard-working junior developer eager
+          to sharpen my coding skills through practice. A quick learner that's
+          open to new challenges, excited about problem solving abstract issues
+          and looking to provide value to a team of developers.
+        </div>
+      </div>
+      <div className="skills">
+        <div>Skills</div>
+        <img class="skills" src="https://imgur.com/s8wa8hC.png" alt="HTML" />
+        <img class="skills" src="https://imgur.com/bwnJcD8.png" alt="CSS" />
+        <img
+          class="skills"
+          src="https://imgur.com/eD8LRpf.png"
+          alt="JavaScript"
+        />
+        <img
+          class="skills"
+          src="https://imgur.com/DPdCHkk.png"
+          alt="React.js"
+        />
+        <img class="skills" src="https://imgur.com/PpW0fLL.png" alt="Node.js" />
+        <img
+          class="skills"
+          src="https://imgur.com/dHxWvof.png"
+          alt="Express.js"
+        />
+        <img class="skills" src="https://imgur.com/xlmAQYr.png" alt="mongoDB" />
+        <img class="skills" src="https://imgur.com/XQQlqqf.png" alt="SQL" />
+        <img class="skills" src="https://imgur.com/M5EcPdg.png" alt="Ruby" />
+        <img
+          class="skills"
+          src="https://imgur.com/0kCTZWS.png"
+          alt="Ruby on Rails"
+        />
+        <img
+          class="skills"
+          src="https://imgur.com/Pp0Qdew.png"
+          alt="postgresql"
+        />
+        <img class="skills" src="https://imgur.com/vXPRpPG.png" alt="git" />
+      </div>
+      <div>
+        <div>
+          <img src="https://i.imgur.com/rwg2z0G.png" alt="project1" />
+          <div>text</div>
+          <button>View Site</button>
+          <button>Github</button>
+        </div>
+        <div>
+          <img src="https://i.imgur.com/4PCwuOR.png" alt="project2" />
+          <div>text</div>
+          <button>View Site</button>
+          <button>Github</button>
+        </div>
+        <div>
+          <img src="https://i.imgur.com/e68Iha4.png" alt="project3" />
+          <div>text</div>
+          <button>View Site</button>
+          <button>Github</button>
+        </div>
+        <div>
+          <img src="https://i.imgur.com/z2qPzWT.png" alt="project4" />
+          <div>text</div>
+          <button>View Site</button>
+          <button>Github</button>
+        </div>
+      </div>
     </div>
   );
 }
